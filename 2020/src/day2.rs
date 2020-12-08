c212d4eead4bcf4fe68354129a6ad93cc516fb14
4rtzel <4rtzel@gmail.com>
2020-12-08 11:50:24 +0300

    Day 2

--- 2020/src/day2.rs
+++ 2020/src/day2.rs
@@ -2,34 +2,34 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 
-fn part_one(numbers: &Vec<u32>) {
-    for num1 in numbers.iter() {
-        for num2 in numbers.iter() {
-            if num1 + num2 == 2020 {
-                println!("{}", num1 * num2);
-                return;
-            }
-        }
-    }
-}
-
-fn part_two(numbers: &Vec<u32>) {
-    for num1 in numbers.iter() {
-        for num2 in numbers.iter() {
-            for num3 in numbers.iter() {
-                if num1 + num2 + num3== 2020 {
-                    println!("{}", num1 * num2 * num3);
-                    return;
-                }
-            }
-        }
-    }
-}
+use regex;
 
 pub fn run(filename: &String) {
     let file = fs::File::open(filename).expect("Unable to open a file");
     let reader = io::BufReader::new(file);
-    let numbers: Vec<u32> = reader.lines().map(|x| x.unwrap().parse::<u32>().unwrap()).collect();
-    part_one(&numbers);
-    part_two(&numbers);
+    let re = regex::Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let mut valid_count = 0;
+    let mut valid_count2 = 0;
+
+    for line in reader.lines() {
+        for cap in re.captures_iter(&line.unwrap()) {
+            let min = cap[1].parse::<usize>().unwrap();
+            let max = cap[2].parse::<usize>().unwrap();
+            let count = cap[4].matches(&cap[3]).count();
+
+            // part one
+            if count >= min && count <= max {
+                valid_count += 1;
+            }
+
+            // part two
+            let ch = cap[3].as_bytes()[0];
+            if ((cap[4].as_bytes()[min - 1] == ch) as u8 ^ (cap[4].as_bytes()[max - 1] == ch) as u8) == 1 {
+                valid_count2 += 1;
+            }
+
+        }
+    }
+    println!("{}", valid_count);
+    println!("{}", valid_count2);
 }
